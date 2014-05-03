8edd713292adb87fb9fab86baa591df4c839d170
foobarbecue <foobarbecue@gmail.com>
2014-05-03 06:39:39 +0000

    Create details box. TODO: figure out how to stick it to the bottom of screen. Maybe look at css in erebuscaves database.

--- main.js
+++ main.js
@@ -1,17 +1,28 @@
 if (Meteor.isClient) {
    Meteor.subscribe("labels");
    Meteor.subscribe("climbs");
-    
+   Session.set("selectedLabel",null)
    Template.controlPanel.labels = function() {
        return Labels.find();
    }
    Template.labels3D.labels = function() {
        return Labels.find();
    }
-   Template.labels3D.climbs= function() {
-       return Climbs.find();
-   }
-   
+   Template.labels3D.events({
+       'mouseenter .label3D': function(event) {
+           Session.set("selectedLabel", event.currentTarget.id);
+           $(event.currentTarget).addClass('selected');
+           $('.label3Ddetails').fadeIn();  
+       },
+       'mouseleave .label3D': function(event) {
+           $(event.currentTarget).removeClass('selected');
+           $('.label3Ddetails').fadeOut();
+       }
+   })
+   Template.label3Ddetails.label = function(){
+       try{return Labels.findOne(Session.get("selectedLabel"))}
+       catch(TypeError){return null}
+   }   
 }
 
 if (Meteor.isServer) {
@@ -21,7 +32,6 @@ if (Meteor.isServer) {
     Meteor.publish("OwnLabels", function() {
          return Labels.find();
     });
-
     Meteor.publish("climbs", function() {
          return Climbs.find();
     });    
