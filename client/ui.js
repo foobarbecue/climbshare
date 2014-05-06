2fdaf2d4cbfd653a293f835f15ca124b4bb98d94
foobarbecue <ubuntu@ip-172-31-36-130.us-west-2.compute.internal>
2014-05-06 04:29:43 +0000

    Add little triangles for menu expansion.

--- client/ui.js
+++ client/ui.js
@@ -2,9 +2,10 @@ $(function() {
     $('#controlPanel').click(
         function(e){e.stopImmediatePropagation();}
                        );
+    $('#controlPanel>h3').append('<span class="expandArrow">&#x25B6</span><span class="expandedArrow hidden">&#x25BC</span>')
     $('#controlPanel>h3').click(
         function(){
-            $(this).next().slideToggle();
+            $(this).next('div').slideToggle();
+            $(this).children('.expandArrow, .expandedArrow').toggle();
     });
-    $('.hidden').hide();
 });
