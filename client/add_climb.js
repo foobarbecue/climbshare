df06d70eb89225d0e7aefec9997e88992ba9c1a6
Aaron Curtis <foobarbecue@gmail.com>
2014-06-06 19:33:10 -1000

    Use a session variable to keep track of current editing "tool" represented by mouse3D.

--- client/add_climb.js
+++ client/add_climb.js
@@ -1,10 +1,14 @@
 function addNewClimb(){
-    boulder = Boulders.findOne({name:Session.get('loadedBoulder')})
-    return Climbs.insert({
+    boulder = Boulders.findOne({name:Session.get('loadedBoulder')});
+    newClimb = Climbs.insert({
         climbName: 'new climb',
         boulder_id: boulder._id,
         createdBy:Meteor.userId(),
-    })
+    });
+    if !!newClimb{
+        Session.set('addClimbVertices')
+        return newClimb
+    }
 }
 
 function addVertexToClimb(climb){
