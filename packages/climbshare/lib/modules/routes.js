1f721cee37bc23739915053fe393952b25735a1c
Aaron Curtis <foobarbecue@gmail.com>
2018-09-15 17:11:21 -0700

    Add threejs

--- packages/climbshare/lib/modules/routes.js
+++ packages/climbshare/lib/modules/routes.js
@@ -1,5 +1,7 @@
 import { addRoute, Components } from 'meteor/vulcan:core'
 
 import '../components/crags/CragItem.jsx'
+import '../components/ThreeScene.jsx'
 
-addRoute({ name: 'crags', path: '/', componentName: 'CragItem'});
+addRoute({ name: 'crags', path: '/crags', componentName: 'CragItem'});
+addRoute({ name: 'threescene', path: '/', componentName: 'ThreeScene'});
