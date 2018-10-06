0c833d44486ee9722f247af3e06863b2083e98fd
Aaron Curtis <foobarbecue@gmail.com>
2018-10-05 23:56:28 -0700

    Add tool icons

--- packages/climbshare/lib/components/common/Toolbox.jsx
+++ packages/climbshare/lib/components/common/Toolbox.jsx
@@ -1,12 +1,15 @@
 import React, {Component} from 'react';
 import {Components, registerComponent, withSingle, withCurrentUser} from 'meteor/vulcan:core';
 
+const tools = ['addClimb','addBeta','addWarning','addOther'];
+
 class Toolbox extends Component {
   render = () =>
 
     <div className={"toolbox-pane"}>
-      <span className={"pane-header"}>Toolbox</span>
-      <p>Will be collapsable and filled with tool icons and such.</p>
+      {tools.map((toolName)=>
+        <img className={'toolIcon'} src={'/tool_icons/'+toolName+'.png'} key={toolName}/>
+      )}
     </div>
 }
 
