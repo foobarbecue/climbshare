31e06cac8335899011e432900c695aeba9ad8d81
Aaron Curtis <foobarbecue@gmail.com>
2018-11-11 18:30:24 -0800

    Update instructions for recent change

--- packages/climbshare/lib/modules/tools.js
+++ packages/climbshare/lib/modules/tools.js
@@ -1,10 +1,10 @@
 const tools = [
   {name: null,
-    instructions: 'Left mouse button to rotate, right to pan, wheel to zoom'},
+    instructions: 'Left mouse button: rotate. Right: pan. Wheel: zoom. Choose a tool from the toolbox at left.'},
   {name: 'addClimb',
     instructions: 'Click on the rock to add a climb.'},
   {name: 'drawClimb',
-    instructions: 'Left click to add a vertex to the climb. Right click to save.'},
+    instructions: 'Left click to add a vertex to the climb. Double click to save.'},
   {name: 'addBeta',
     instructions: 'Not yet implemented.'},
   {name: 'addWarning',
